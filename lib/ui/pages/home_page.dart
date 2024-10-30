1e2c462e3d7100812832c7186a8b16664c2ad90e
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2024-10-31 01:27:40 +0900

    display pokemon image

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,8 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pokemon_app_flutter_web/infrastracture/model/pokemon_model.dart';
-import 'package:pokemon_app_flutter_web/infrastracture/provider/api_client.dart';
 import 'package:pokemon_app_flutter_web/provider/pokemons_provider.dart';
 
 class MyWidget extends HookConsumerWidget {
@@ -41,6 +40,11 @@ class _PokemonListView extends StatelessWidget {
         itemCount: pokemonModels.length,
         itemBuilder: (context, index) {
           return ListTile(
+            leading: SizedBox(
+              height: 50,
+              width: 50,
+              child: CachedNetworkImage(imageUrl: pokemonModels[index].sprites.front_default),
+            ),
             title: Text(pokemonModels[index].name),
           );
         },
